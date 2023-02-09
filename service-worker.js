e5a5e41d223092db6400e061139ab1efeee2a782
Dom Jones <73879543+jonesd504@users.noreply.github.com>
2023-02-09 16:07:10 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -29,6 +29,24 @@ self.addEventListener('activate', function(e) {
   );
 });
 
+window.addEventListener('beforeinstallprompt', function(event) {
+  event.preventDefault();
+  var installPrompt = event;
+  var installButton = document.getElementById('install-button');
+  installButton.addEventListener('click', function() {
+    installPrompt.prompt();
+    installPrompt.userChoice.then(function(choice) {
+      if (choice.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      installPrompt = null;
+    });
+  });
+  installButton.style.display = 'block';
+});
+
 self.addEventListener('fetch', function(e) {
   console.log('[ServiceWorker] Fetch', e.request.url);
   e.respondWith(
